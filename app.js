79979a71a8d26e3f508d5109ee37653425e1817b
Felipe Miosso <metta5@metta5.com>
2022-06-27 21:27:21 -0300

    adicionando funcao de save para seasonType

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const { DB }  = require('./db');
+const { DB }  = require("./db");
 const { Player } = require("./entities/player");
+const { Match } = require("./entities/match");
+const { Season } = require("./entities/season");
+const { SeasonType } = require("./entities/seasonType");
 
-const p1 = new Player("Felipe", "Miosso", []);
-DB.players.push(p1);
+const sType1 = new SeasonType("par/impar", "individual");
+const sType2 = new SeasonType("par/impar", "dupla");
 
-console.log(DB.players);
+DB.saveSeasonType(sType1);
+DB.saveSeasonType(sType2);
+
+// const s1 = new Season()
+// 
+// const p1 = new Player("P1", "P1", []);
+// const p2 = new Player("P2", "P2", []);
+// 
+// const m1 = new Match()
+// 
+// DB.players.push(p1);
+// DB.players.push(p2);
--- db.js
+++ db.js
@@ -7,6 +7,11 @@ function DB() {
     this.seasonRankingPlayers = [];
     this.seasonTypes = [];
 
+    this.saveSeasonType = function(seasonType) {
+        seasonType.id = this.seasonTypes.length + 1;
+        this.seasonTypes.push(seasonType);
+    }
+
     return this;
 }
 
